21c366ddaf678f832c272dcb4a0621e9d96731d8
Prakash V S <141955456+Prakash-V-S@users.noreply.github.com>
2024-03-03 11:24:54 +0530

    Update Products.jsx

--- src/Products.jsx
+++ src/Products.jsx
@@ -11,13 +11,7 @@ export default [
         "brand": "Apple",
         "category": "smartphones",
         "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-        "images": [
-            "https://i.dummyjson.com/data/products/1/1.jpg",
-            "https://i.dummyjson.com/data/products/1/2.jpg",
-            "https://i.dummyjson.com/data/products/1/3.jpg",
-            "https://i.dummyjson.com/data/products/1/4.jpg",
-            "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
-        ]
+        "images": "https://im.indiatimes.in/content/2020/Mar/iPhone-9-and-9-plus_5e71d66e079f2.jpg?w=1200&h=900&cc=1"
     },
     {
         "id": 2,
@@ -30,12 +24,7 @@ export default [
         "brand": "Apple",
         "category": "smartphones",
         "thumbnail": "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
-        "images": [
-            "https://i.dummyjson.com/data/products/2/1.jpg",
-            "https://i.dummyjson.com/data/products/2/2.jpg",
-            "https://i.dummyjson.com/data/products/2/3.jpg",
-            "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
-        ]
+        "images": 'https://m.media-amazon.com/images/I/61HHbO9NDFL.jpg'
     },
     {
         "id": 3,
@@ -48,9 +37,7 @@ export default [
         "brand": "Samsung",
         "category": "smartphones",
         "thumbnail": "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
-        "images": [
-            "https://i.dummyjson.com/data/products/3/1.jpg"
-        ]
+        "images": 'https://admin.hyperce.io/assets/thumbnail__12__preview.jpg?w=800'
     },
     {
         "id": 4,
@@ -63,13 +50,7 @@ export default [
         "brand": "OPPO",
         "category": "smartphones",
         "thumbnail": "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
-        "images": [
-            "https://i.dummyjson.com/data/products/4/1.jpg",
-            "https://i.dummyjson.com/data/products/4/2.jpg",
-            "https://i.dummyjson.com/data/products/4/3.jpg",
-            "https://i.dummyjson.com/data/products/4/4.jpg",
-            "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
-        ]
+        "images": 'https://5.imimg.com/data5/SELLER/Default/2023/6/316894377/HW/EM/ZV/157629195/oppo-f19-pro-plus-5g-8-128gb-.jpg'
     },
     {
         "id": 5,
@@ -82,10 +63,6 @@ export default [
         "brand": "Huawei",
         "category": "smartphones",
         "thumbnail": "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
-        "images": [
-            "https://i.dummyjson.com/data/products/5/1.jpg",
-            "https://i.dummyjson.com/data/products/5/2.jpg",
-            "https://i.dummyjson.com/data/products/5/3.jpg"
-        ]
+        "images": 'https://5.imimg.com/data5/ZR/TV/HW/SELLER-57126529/huawei-p30-pro-6-gb-ram-plus-128-gb-rom-pearl-white-mobile-500x500.jpg'
     }
 ]
